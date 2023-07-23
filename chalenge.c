1af69b31578ff4fc70cedb70fe5259a321ca8e35
ElroiCarmel <elroica277@gmail.com>
2023-07-23 19:38:22 +0300

    Couiple added

--- chalenge.c
+++ chalenge.c
@@ -5,5 +5,10 @@ int main()
 {
     printf("I am %d years old and next year I will be %d years old\n", 26, 27);
     printf("My average grade is %.2f\n", 83.35);
+    printf("Year = %d\n", 2023);
+    printf("num1 = %d, num2 = %d, sum = %d\n", 5,7,5+7);
+    printf("%.1f\n", 12.0/5.0);
+    printf("The remainder of %d %% %d is %d \n",5,2,5%2);
+    printf("Hey // Alpha \n");
     return 0;
 }
