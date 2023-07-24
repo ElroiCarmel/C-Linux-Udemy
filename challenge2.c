2a73c0ecad1b174c45af7af58f0dbe845c5738bb
ElroiCarmel <elroica277@gmail.com>
2023-07-24 15:13:54 +0300

    Upload

---
+++ challenge2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int year;
+    int age;
+    // printf("What is the year now? ");
+    scanf("Year: %d\n", &year);
+    // printf("What is your age? ");
+    scanf("%d\n", &age);
+    return 0;
+}
