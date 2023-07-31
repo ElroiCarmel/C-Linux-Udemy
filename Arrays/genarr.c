fa931034714c10a730ed957df8d94eb048558cbf
ElroiCarmel <elroica277@gmail.com>
2023-07-31 14:13:27 +0300

    end of arrays basic

---
+++ Arrays/genarr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int grades[5];
+    int i, lowest;
+    for (i=0;i<5;i++)
+    {
+        printf("Enter grade: ");
+        scanf("%d", &grades[i]);
+    }
+    lowest = grades[0];
+    for (i=1;i<5;i++)
+    {
+        if (grades[i]<lowest) lowest = grades[i];
+    }
+    printf("The lowest grade is: %d\n", lowest);
+    printf("Your grades are: %d\n", grades);
+    return 0;
+}
