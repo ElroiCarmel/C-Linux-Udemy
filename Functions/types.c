7404d31266064a40fd90215ade5eff7b7dfe5133
ElroiCarmel <elroica277@gmail.com>
2023-07-31 09:16:36 +0300

    update functions basic

--- Functions/types.c
+++ Functions/types.c
@@ -48,14 +48,25 @@ int maxBetween3()
     return num3;
 }
 
+double findAve(int g1, int g2, int g3)
+{
+    double ave;
+    ave = (g1+g2+g2)/3.0;
+    return ave;
+}
+
 int main()
 {
     int max;
+    int g1,g2,g3;
     message();
     printMinMax(4,10);
     printf("Maximum is: %d\n", getMax(10,5,8));
     max = maxBetween3();
     printf("MAx between three is: %d\n", max);
+    printf("Enter 3 grades: ");
+    scanf("%d%d%d", &g1,&g2,&g3);
+    printf("The average is : %.2f\n", findAve(g1,g2,g3));
     return 0;
 }
 
